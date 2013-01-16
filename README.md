c3e79e9f1b1cc596ff4ec821b5f7965f9edc088a
Orazio Briante <orazio.briante@hotmail.it>
2013-01-16 14:03:19 +0100

    Public Release
    Signed-off-by: Orazio Briante <orazio.briante@hotmail.it>

---
+++ README.md
@@ -0,0 +1,14 @@
+# XBeeAPI - Arduino library 
+### Copyright (C) 2013 - Dott. Orazio Briante <orazio.briante@unirc.it>
+### Laboratory A.R.T.S. - University Mediterranea of Reggio Calabria - Faculty of Engineering
+### Via Graziella Feo di Vito I - 89100 Reggio Calabria (RC), Italy
+
+### The arduino XBeeAPI library facilitate the management, of XBee Devices configured in API Mode
+
+## Requirements:
+
+* xbee-arduino library - http://code.google.com/p/xbee-arduino/
+
+
+###NOTE:
+The XBee Devices may be externally configured (es. with X-CTU program).
--- XBeeAPIDebug.h
+++ XBeeAPIDebug.h
@@ -1,8 +1,21 @@
 /*
- * XBeeAPIDebug.h
+ * The arduino XBeeAPI library facilitate the management, of XBee Devices configured in API Mode
+ * Copyright (C) 2013  Orazio Briante orazio.briante@unirc.it
+ * Laboratory A.R.T.S. - University Mediterranea of Reggio Calabria - Faculty of Engineering
  *
- *  Created on: Oct 21, 2012
- *      Author: kmonkey
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef XBEEAPIDEBUG_H_
